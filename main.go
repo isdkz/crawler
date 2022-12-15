509898bba556712d500c4d0e85793a554fd5eab2
isdkz <dkz6174510@gmail.com>
2022-12-15 14:55:03 +0800

    v0.0.5

--- main.go
+++ main.go
@@ -1,33 +1,62 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"regexp"
+
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
 )
 
-func main() {
-	url := "https://www.thepaper.cn/"
+func Fetch(url string) ([]byte, error) {
+
 	resp, err := http.Get(url)
 
 	if err != nil {
-		fmt.Printf("fetch url error:%v", err)
-		return
+		panic(err)
 	}
 
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("Error status code:%v", resp.StatusCode)
-		return
+		fmt.Printf("Erro status code:%d", resp.StatusCode)
 	}
+	bodyReader := bufio.NewReader(resp.Body)
+	e := DeterminEncoding(bodyReader)
+	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
+	return ioutil.ReadAll(utf8Reader)
+}
 
-	body, err := ioutil.ReadAll(resp.Body)
+func DeterminEncoding(r *bufio.Reader) encoding.Encoding {
+
+	bytes, err := r.Peek(1024)
 
 	if err != nil {
-		fmt.Println("read content failed:%v", err)
-		return
+		fmt.Println("fetch error:%v", err)
+		return unicode.UTF8
 	}
 
-	fmt.Println("body:", string(body))
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
+}
+
+var headerRe = regexp.MustCompile(`<div class="small_cardcontent__BTALp".*?<h2>(.*?)</h2>`)
+
+func main() {
+	url := "https://www.thepaper.cn/"
+	body, err := Fetch(url)
+
+	if err != nil {
+		fmt.Printf("read content failed:%v", err)
+		return
+	}
+	matches := headerRe.FindAllSubmatch(body, -1)
+	for _, m := range matches {
+		fmt.Println("fetch card news:", string(m[1]))
+	}
 }
