1ac72f4650c64d3ed812f9344ea76d8ebd0ec18e
isdkz <dkz6174510@gmail.com>
2022-12-17 21:03:11 +0800

    use headers

--- go.mod
+++ go.mod
@@ -3,9 +3,6 @@ module github.com/isdkz/crawler
 go 1.19
 
 require (
-	github.com/PuerkitoBio/goquery v1.8.0
 	golang.org/x/net v0.2.0
 	golang.org/x/text v0.4.0
 )
-
-require github.com/andybalholm/cascadia v1.3.1 // indirect
--- main.go
+++ main.go
@@ -1,69 +1,19 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
 	"fmt"
-	"io/ioutil"
-	"net/http"
 
-	"github.com/PuerkitoBio/goquery"
-	"golang.org/x/net/html/charset"
-	"golang.org/x/text/encoding"
-	"golang.org/x/text/encoding/unicode"
-	"golang.org/x/text/transform"
+	"github.com/isdkz/crawler/collect"
 )
 
-func Fetch(url string) ([]byte, error) {
-
-	resp, err := http.Get(url)
-
-	if err != nil {
-		panic(err)
-	}
-
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("Erro status code:%d", resp.StatusCode)
-	}
-	bodyReader := bufio.NewReader(resp.Body)
-	e := DeterminEncoding(bodyReader)
-	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
-	return ioutil.ReadAll(utf8Reader)
-}
-
-func DeterminEncoding(r *bufio.Reader) encoding.Encoding {
-
-	bytes, err := r.Peek(1024)
-
-	if err != nil {
-		fmt.Println("fetch error:%v", err)
-		return unicode.UTF8
-	}
-
-	e, _, _ := charset.DetermineEncoding(bytes, "")
-	return e
-}
-
 func main() {
-	url := "https://www.thepaper.cn/"
-	body, err := Fetch(url)
+	url := "https://book.douban.com/subject/1007305/"
 
+	var f collect.Fetcher = collect.BrowserFetch{}
+	body, err := f.Get(url)
 	if err != nil {
 		fmt.Printf("read content failed:%v", err)
 		return
 	}
-
-	// 加载HTML文档
-	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(body))
-	if err != nil {
-		fmt.Println("read content failed:%v", err)
-		return
-	}
-
-	doc.Find("a.index_inherit__A1ImK h2").Each(func(i int, s *goquery.Selection) {
-		title := s.Text()
-		fmt.Printf("Review %d: %s\n", i, title)
-	})
+	fmt.Println(string(body))
 }
